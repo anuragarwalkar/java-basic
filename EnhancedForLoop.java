bbc75d88227f7f600a8b4ef45593a1d1c0e6fa9b
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-25 01:44:13 +0530

    added enhanced for loop

---
+++ EnhancedForLoop.java
@@ -0,0 +1,9 @@
+public class EnhancedForLoop {
+    public static void main(String args[]) {
+        int a[] = {1, 2, 3, 4};
+
+        for(int k : a) {
+            System.out.print(k + ": JavaScript!!! ");
+        }
+    }
+}

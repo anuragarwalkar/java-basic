142a8c32eef671e05ad3a553c296901b7939a9f1
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-26 00:15:11 +0530

    mt

--- src/MultiThreadingDemo.java
+++ src/MultiThreadingDemo.java
@@ -9,11 +9,49 @@ class MyThread extends Thread {
         }
     }
 }
+
+class Hi extends Thread{
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hi");
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+        }
+    }
+}
+
+class Hello extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+        }
+    }
+}
 public class MultiThreadingDemo {
     
     public static void main(String[] args) {
-        MyThread t1 = new MyThread();
-        t1.start();
+        // MyThread t1 = new MyThread();
+        // t1.start();
+
+        Hi obj = new Hi();
+        Hello obj2 = new Hello();
+
+        obj.start();
+        obj2.start();
+
+
+        // obj.show();
+        // obj2.show();
+
     }
     
 }

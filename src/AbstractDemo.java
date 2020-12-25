fdc4006d1d8abb16e5a516ad54de19c0bfc38700
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-25 12:50:14 +0530

    adde final class

---
+++ src/AbstractDemo.java
@@ -0,0 +1,26 @@
+package src;
+
+abstract class Human {
+    public abstract void eat();
+
+    public abstract void walk();
+}
+
+
+class Man extends Human {
+    public void eat() {
+
+    }
+
+    public void walk() {
+        
+    }
+}
+
+
+
+public class AbstractDemo {
+    public static void main(String[] args) {
+        // Human obj = new Human(); We cant Intantiate obj
+    }
+}
---
+++ src/FinalDemo.java
@@ -0,0 +1,22 @@
+package src;
+
+// If you make final to a class 
+final class A {
+    public void show() {
+        System.out.println("In show");
+    }
+}
+
+// Then you can't extend that class
+class B extends A {
+
+}
+
+
+public class FinalDemo {
+    public static void main(String[] args) {
+        B obj = new B();
+
+        obj.show();
+    }
+}

80914578aec494ef38f47af8f466ac90fd4cc5f3
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-26 13:55:50 +0530

    custom comparitor

--- src/CollectionDemo.java
+++ src/CollectionDemo.java
@@ -2,6 +2,8 @@ package src;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
@@ -15,19 +17,47 @@ import java.util.TreeSet;
 
 // IMP Collection does not work with index
 
+
 public class CollectionDemo {
     public static void main(String[] args) {
         // Collection values = new ArrayList();  // Add any values
         // values.add(2);
         // values.add("anruag");
 
-        Collection values = new ArrayList<>();
+        // Collection values = new ArrayList<>();
 
+        List values = new ArrayList(); //Mutable
+
+        values.add(9);
         values.add(5);
         values.add(6);
         values.add(7);
 
-        Iterator it = values.iterator();
+        Comparator<Integer> c = new Comparator<Integer>(){
+            public int compare(Integer i, Integer j) {
+               if (i % 10 > j% 10) {
+                   return 1;
+               } else {
+                   return -1;
+               }
+            }
+        };
+
+        Collections.sort(values, c);
+
+        Collections.reverse(values);
+
+        System.out.println(Collections.min(values));
+
+        // for(int i = 0; i < values.size(); i++) {
+        //     System.out.println(values.get(i));
+        // }
+
+        // Iterator it = values.iterator();
+
+        for(Object o: values) {
+            System.out.println(o);
+        }
 
 
         // Collection<Integer> values = new ArrayList<Integer>(); // Generics Before 1.7
@@ -43,9 +73,9 @@ public class CollectionDemo {
 
         // values.add(2);
 
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
+        // while (it.hasNext()) {
+        //     System.out.println(it.next());
+        // }
 
     }
 }

d7945365dcd4194563efc5d1a1fcd7949d59ec19
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-26 11:12:02 +0530

    colleaction api all

--- src/CollectionDemo.java
+++ src/CollectionDemo.java
@@ -2,9 +2,13 @@ package src;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 // C => List => ArrayList
 
@@ -18,13 +22,18 @@ public class CollectionDemo {
 
         // Collection<Integer> values = new ArrayList<Integer>(); // Generics Before 1.7
         // Collection<Integer> values = new ArrayList<>(); // Generics 1.7
-        List<Integer> values = new ArrayList<>(); // Generics 1.7
+        // List<Integer> values = new ArrayList<>(); // Generics 1.7
 
-        // Set<Integer> values = new HashSet<>(); // Uniq values
+        // Set<Integer> values = new HashSet<>(); // Uniq values but while fetching data get random order
+        Set<Integer> values = new TreeSet<>(); // Uniq values but while fetching data get sorted order
+
+        Map<Integer, String> m = new HashMap();
+        // Map<Integer, String> m = new Hashtable<>(); Thread safe sync
+
 
         values.add(2);
 
-        System.out.println(values.get(0));
+        System.out.println(values);
 
     }
 }

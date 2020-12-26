48a5e39017de13f78d72dc9d737b5ebe88295027
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-26 14:56:45 +0530

    added hash table

--- src/MapDemo.java
+++ src/MapDemo.java
@@ -1,12 +1,15 @@
 package src;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.Set;
 
 public class MapDemo {
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
+        // Map<String, String> map = new HashMap<>();
+        Map<String, String> map = new Hashtable<>(); // Thread safe
+
 
         map.put("name", "anurag");
         map.put("lastName", "arwalkar");

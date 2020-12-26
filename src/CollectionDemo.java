63cb58a1b0936296e0ea9586d046cd5b101ac00a
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-26 13:06:22 +0530

    Iterator

--- src/CollectionDemo.java
+++ src/CollectionDemo.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -20,20 +21,31 @@ public class CollectionDemo {
         // values.add(2);
         // values.add("anruag");
 
+        Collection values = new ArrayList<>();
+
+        values.add(5);
+        values.add(6);
+        values.add(7);
+
+        Iterator it = values.iterator();
+
+
         // Collection<Integer> values = new ArrayList<Integer>(); // Generics Before 1.7
         // Collection<Integer> values = new ArrayList<>(); // Generics 1.7
         // List<Integer> values = new ArrayList<>(); // Generics 1.7
 
         // Set<Integer> values = new HashSet<>(); // Uniq values but while fetching data get random order
-        Set<Integer> values = new TreeSet<>(); // Uniq values but while fetching data get sorted order
+        // Set<Integer> values = new TreeSet<>(); // Uniq values but while fetching data get sorted order
 
-        Map<Integer, String> m = new HashMap();
+        // Map<Integer, String> m = new HashMap();
         // Map<Integer, String> m = new Hashtable<>(); Thread safe sync
 
 
-        values.add(2);
+        // values.add(2);
 
-        System.out.println(values);
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
 
     }
 }

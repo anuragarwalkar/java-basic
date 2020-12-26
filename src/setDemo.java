6ddde29f8c6835dfd47747c4dabef82038185f32
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-26 14:46:56 +0530

    added set class

---
+++ src/setDemo.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class setDemo {
+    public static void main(String[] args) {
+        // Set<Integer> values = new HashSet<>(); // Sequence is not maintained
+        Set<Integer> values = new TreeSet<>(); // Sequence is maintained
+
+        values.add(5);
+        values.add(6);
+        values.add(8);
+        values.add(0);
+        values.add(89);
+
+        for(int i : values) {
+            System.out.println(i);
+        }
+    }
+}

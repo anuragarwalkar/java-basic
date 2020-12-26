cd9a2929f65196214f25616036a1c82612f1619a
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-26 14:54:34 +0530

    added hash map

---
+++ src/MapDemo.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("name", "anurag");
+        map.put("lastName", "arwalkar");
+
+
+        // System.out.println(map);
+
+        // System.out.println(map.get("name"));
+
+        Set<String> keys = map.keySet();
+
+        for (String key : keys) {
+            System.out.println(map.get(key));
+        }
+
+    }
+}

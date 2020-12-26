25c7bc38e4e64a58a709bd9c2362b8c4a90385de
Anurag Arwalkar <anurag.arwalkar@synerzip.com>
2020-12-26 11:04:44 +0530

    added list and set

--- src/CollectionDemo.java
+++ src/CollectionDemo.java
@@ -2,6 +2,13 @@ package src;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+// C => List => ArrayList
+
+// IMP Collection does not work with index
 
 public class CollectionDemo {
     public static void main(String[] args) {
@@ -10,11 +17,14 @@ public class CollectionDemo {
         // values.add("anruag");
 
         // Collection<Integer> values = new ArrayList<Integer>(); // Generics Before 1.7
-        Collection<Integer> values = new ArrayList<>(); // Generics 1.7
+        // Collection<Integer> values = new ArrayList<>(); // Generics 1.7
+        List<Integer> values = new ArrayList<>(); // Generics 1.7
+
+        // Set<Integer> values = new HashSet<>(); // Uniq values
 
         values.add(2);
 
-        System.out.println(values);
+        System.out.println(values.get(0));
 
     }
 }

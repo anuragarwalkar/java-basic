9e9f176e398cfb3bf8a6bb353413b6954f27b8f4
Anurag <anuragarwalkar@gmail.com>
2021-02-09 22:39:48 +0530

    added comment to final

---
+++ src/com/demo/basic/primitive/variables/FinalDemo.java
@@ -0,0 +1,9 @@
+package src.com.demo.basic.primitive.variables;
+
+public class FinalDemo {
+    public static void main(String[] args) {
+        final int DEMO = 0;
+        // demo = 1 // Final means a constant variable you can't change the variable
+        System.out.println(DEMO);
+    }
+}

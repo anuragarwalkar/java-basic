47bfc2aa71efab5f53c6122a879a54ad13736df2
Anurag <anuragarwalkar@gmail.com>
2021-02-09 22:35:27 +0530

    added array methods

--- src/com/demo/basic/referance/ArraysDemo.java
+++ src/com/demo/basic/referance/ArraysDemo.java
@@ -11,10 +11,29 @@ public class ArraysDemo {
         // Add numbers
         numbers[0] = 1;
 
+        int[][] twoDimentionalArray = new int[2][3];
+        int[][] twoDimentionalArrayNew = { { 1, 2 }, { 1, 2, 3 } };
+
+        twoDimentionalArray[0][0] = 1;
+
+        System.out.println(Arrays.deepToString(twoDimentionalArray));
+
+        int[][][] threeDimentionalArray = new int[2][3][4];
+
+        threeDimentionalArray[0][0][0] = 2;
+
+        System.out.println(Arrays.deepToString(threeDimentionalArray));
+
         // Initalise the array if we know the values AOT
-        int[] numbersNew = { 1, 2 };
+        int[] numbersNew = { 1, 2, 5, 4, 9, 7, 8 };
 
         System.out.println(Arrays.toString(numbers));
+
+        System.out.println(Arrays.toString(numbersNew));
+
+        System.out.println(numbersNew.length);
+
+        Arrays.sort(numbersNew);
 
         System.out.println(Arrays.toString(numbersNew));
     }
